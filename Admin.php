8dda3de6d30bd28b5bd7ab078ea7b194592ba4c8
rohan yadav <68214716+R0han-Yadav@users.noreply.github.com>
2022-05-05 22:15:55 +0700

    Create Admin.php

---
+++ Admin.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Welcom​e​">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Admin</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Admin.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 4.5.4, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/ansal_university-removebg-preview.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Admin">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body u-xl-mode"><header class="u-black u-clearfix u-header u-header" id="sec-2a68"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="https://nicepage.com" class="u-image u-logo u-image-1" data-image-width="150" data-image-height="100">
+          <img src="images/ansal_university-removebg-preview.png" class="u-logo-image u-logo-image-1">
+        </a>
+      </div></header>
+    <section class="u-align-center u-clearfix u-grey-50 u-section-1" id="sec-b544">
+      <div class="u-align-left u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <h1 class="u-align-center u-text u-text-default u-text-1">Welcom​e​<span class="u-text-palette-4-light-2"></span><span class="u-text-palette-4-light-2"></span>
+        </h1>
+      </div>
+    </section>
+    <section class="u-clearfix u-grey-75 u-section-2" id="sec-ae31">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <div class="u-align-left u-border-2 u-border-palette-4-light-2 u-expanded-width u-left-0 u-shape u-shape-rectangle u-shape-1"></div>
+        <div class="u-clearfix u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-row">
+              <div class="u-align-center-sm u-align-center-xs u-align-left-lg u-align-left-md u-align-left-xl u-container-style u-layout-cell u-left-cell u-size-43 u-layout-cell-1">
+                <div class="u-container-layout u-valign-middle u-container-layout-1">
+                  <h3 class="u-text u-text-default u-text-1">View inventory&nbsp;<br>
+                  </h3>
+                  <p class="u-text u-text-2">view current inventory and stock of selected items&nbsp;<br>
+                  </p>
+                </div>
+              </div>
+              <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-17 u-layout-cell-2">
+                <div class="u-container-layout u-valign-middle u-container-layout-2">
+                  <a href="inventory.html" data-page-id="330827972" class="u-border-none u-btn u-button-style u-hover-palette-4-dark-1 u-palette-4-light-2 u-btn-1">go to inventory</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-image u-shading u-section-3" id="sec-cea8" data-image-width="600" data-image-height="360">
+      <br><br><br>
+      
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        
+        <div class="u-expanded-width u-list u-list-1">
+          
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-95df"><div class="u-clearfix u-sheet u-sheet-1">
+      <p class="u-small-text u-text u-text-variant">&copy; Rohan Yadav | Build version: 1.0.1 | Last Updated: <?php echo date("d-m-Y"); ?></p>
+    </div></footer>
+  </body>
+</html>
